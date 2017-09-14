09e283f48095959fba56237547b822046bde6b3f
charlesRollandy <charles.rollandy@gmail.com>
2017-09-14 14:11:03 +0200

    Refactored after passing test 5

--- BerlinClock.UnitTests/TimeConverterTests.cs
+++ BerlinClock.UnitTests/TimeConverterTests.cs
@@ -7,10 +7,17 @@ namespace BerlinClock.UnitTests
     [TestClass]
     public class TimeConverterTests
     {
+        private TimeConverter timeConverter;
+
+        [TestInitialize]
+        public void Init()
+        {
+            timeConverter = new TimeConverter();
+        }
+
         [TestMethod]
         public void Should_Yellow_Lamp_On_Top_Be_Illuminated_When_Seconds_Are_Even()
         {
-            TimeConverter timeConverter = new TimeConverter();
             Assert.AreEqual("Y", timeConverter.ConvertSecondsToSecondsLampRow(0));
             Assert.AreEqual("Y", timeConverter.ConvertSecondsToSecondsLampRow(2));
             Assert.AreEqual("Y", timeConverter.ConvertSecondsToSecondsLampRow(30));
@@ -20,7 +27,6 @@ namespace BerlinClock.UnitTests
         [TestMethod]
         public void Should_Yellow_Lamp_On_Top_Not_Be_Illuminated_When_Seconds_Are_Odd()
         {
-            TimeConverter timeConverter = new TimeConverter();
             Assert.AreEqual("O", timeConverter.ConvertSecondsToSecondsLampRow(1));
             Assert.AreEqual("O", timeConverter.ConvertSecondsToSecondsLampRow(3));
             Assert.AreEqual("O", timeConverter.ConvertSecondsToSecondsLampRow(31));
@@ -30,7 +36,6 @@ namespace BerlinClock.UnitTests
         [TestMethod]
         public void Should_Top_Hours_Row_Have_Four_Lamps()
         {
-            TimeConverter timeConverter = new TimeConverter();
             for (int i = 0; i < 60; i++)
             {
                 Assert.AreEqual(4, timeConverter.ConvertHoursToTopHoursLampRow(i).Length);
@@ -40,15 +45,12 @@ namespace BerlinClock.UnitTests
         [TestMethod]
         public void Should_Top_Hours_Row_Have_No_Red_Lamp_Illuminated_When_Hour_Is_Zero()
         {
-            TimeConverter timeConverter = new TimeConverter();
             Assert.AreEqual("OOOO", timeConverter.ConvertHoursToTopHoursLampRow(0));
         }
 
         [TestMethod]
         public void Should_Every_Top_Hours_Row_Lamp_Be_Illuminated_When_Another_5_Hours_Have_Passed()
         {
-            TimeConverter timeConverter = new TimeConverter();
-
             Assert.AreEqual("OOOO", timeConverter.ConvertHoursToTopHoursLampRow(2));
             Assert.AreEqual("OOOO", timeConverter.ConvertHoursToTopHoursLampRow(4));
             Assert.AreEqual("ROOO", timeConverter.ConvertHoursToTopHoursLampRow(5));
