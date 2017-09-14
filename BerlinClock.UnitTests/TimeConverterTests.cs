0b46e19ded9e4a610608a6cb13e4b3b0d33b507f
charlesRollandy <charles.rollandy@gmail.com>
2017-09-14 13:36:24 +0200

    Created test 2 and checkd it fails

--- BerlinClock.UnitTests/TimeConverterTests.cs
+++ BerlinClock.UnitTests/TimeConverterTests.cs
@@ -16,5 +16,15 @@ namespace BerlinClock.UnitTests
             Assert.AreEqual("Y", timeConverter.ConvertSecondsToSecondsLampRow(30));
             Assert.AreEqual("Y", timeConverter.ConvertSecondsToSecondsLampRow(58));
         }
+
+        [TestMethod]
+        public void Should_Yellow_Lamp_On_Top_Not_Be_Illuminated_When_Seconds_Are_Odd()
+        {
+            TimeConverter timeConverter = new TimeConverter();
+            Assert.AreEqual("O", timeConverter.ConvertSecondsToSecondsLampRow(1));
+            Assert.AreEqual("O", timeConverter.ConvertSecondsToSecondsLampRow(3));
+            Assert.AreEqual("O", timeConverter.ConvertSecondsToSecondsLampRow(31));
+            Assert.AreEqual("O", timeConverter.ConvertSecondsToSecondsLampRow(59));
+        }
     }
 }
