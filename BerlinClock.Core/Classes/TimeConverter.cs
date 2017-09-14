2897eb6b0af101f1150885cfee0c33936bbda05a
charlesRollandy <charles.rollandy@gmail.com>
2017-09-14 14:38:14 +0200

    Refactored after passing test 11

--- BerlinClock.Core/Classes/TimeConverter.cs
+++ BerlinClock.Core/Classes/TimeConverter.cs
@@ -21,31 +21,26 @@ namespace BerlinClock.Core
         {
             int numberOfLightsIlluminated = (hours - (hours % 5)) / 5;
 
-            return ConvertIlluminatedLampsInARowToString(numberOfLightsIlluminated);
+            return ConvertIlluminatedLampsInARowToString(4, numberOfLightsIlluminated);
         }
 
         public string ConvertHoursToBottomHoursLampRow(int hours)
         {
             int numberOfLightsIlluminated = hours % 5;
 
-            return ConvertIlluminatedLampsInARowToString(numberOfLightsIlluminated);
+            return ConvertIlluminatedLampsInARowToString(4, numberOfLightsIlluminated);
         }
 
         public string ConvertMinutesToTopMinutesLampRow(int minutes)
         {
             int numberOfLightsIlluminated = (minutes - (minutes % 5)) / 5;
-            string topMinutesRowResult = string.Empty;
-            for (int i = 1; i <= 11; i++)
-            {
-                topMinutesRowResult += (i <= numberOfLightsIlluminated) ? "R" : "O";
-            }
-            return topMinutesRowResult;
+            return ConvertIlluminatedLampsInARowToString(11, numberOfLightsIlluminated);
         }
 
-        private string ConvertIlluminatedLampsInARowToString(int numberOfLightsIlliminated)
+        private string ConvertIlluminatedLampsInARowToString(int numberOfLampsInTheRow, int numberOfLightsIlliminated)
         {
             string bottomHoursRowResult = string.Empty;
-            for (int i = 1; i <= 4; i++)
+            for (int i = 1; i <= numberOfLampsInTheRow; i++)
             {
                 bottomHoursRowResult += (i <= numberOfLightsIlliminated) ? "R" : "O";
             }
